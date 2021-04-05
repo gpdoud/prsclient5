ab4dd78f03a70d65acfe21842d05ad8267cd04a0
Greg Doud <gpdoud@gmail.com>
2021-04-05 09:30:06 -0400

    Completed the Product components

--- src/app/prod/product-create/product-create.component.ts
+++ src/app/prod/product-create/product-create.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { SystemService } from 'src/app/misc/system.service';
+import { Vendor } from 'src/app/vend/vendor.class';
+import { VendorService } from 'src/app/vend/vendor.service';
+import { Product } from '../product.class';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-create',
@@ -7,9 +13,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductCreateComponent implements OnInit {
 
-  constructor() { }
+  product: Product = new Product();
+  vendors: Vendor[] = [];
 
+  constructor(
+    private sys: SystemService,
+    private prd: ProductService,
+    private vnd: VendorService,
+    private router: Router
+  ) { }
+
+  save(): void {
+    console.debug("B4:", this.product);
+    this.prd.create(this.product).subscribe(
+      res => { console.debug(res); this.router.navigateByUrl("/prod/list"); },
+      err => { console.error(err); }
+      );
+    }
+    
   ngOnInit(): void {
+    this.vnd.list().subscribe(
+      res => { console.debug(res); this.vendors = res; },
+      err => { console.error(err); }
+    );
   }
 
 }
--- src/app/prod/product-edit/product-edit.component.ts
+++ src/app/prod/product-edit/product-edit.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { SystemService } from 'src/app/misc/system.service';
+import { Vendor } from 'src/app/vend/vendor.class';
+import { VendorService } from 'src/app/vend/vendor.service';
+import { Product } from '../product.class';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-edit',
@@ -7,9 +13,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductEditComponent implements OnInit {
 
-  constructor() { }
+  product: Product = null;
+  vendors: Vendor[] = [];
+  
+  constructor(
+    private sys: SystemService,
+    private prd: ProductService,
+    private vnd: VendorService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
+
+  save(): void {
+    console.debug("B4:", this.product);
+    this.prd.change(this.product).subscribe(
+      res => { console.debug(res); this.router.navigateByUrl("/prod/list"); },
+      err => { console.error(err); }
+    );
+  }
 
   ngOnInit(): void {
+    this.sys.chkLogin();
+    this.vnd.list().subscribe(
+      res => { console.debug(res); this.vendors = res; },
+      err => { console.error(err); }
+    );
+    let id = this.route.snapshot.params.id;
+    this.prd.get(+id).subscribe(
+      res => { console.debug(res); this.product = res; },
+      err => { console.error(err); }
+    );
   }
 
 }
